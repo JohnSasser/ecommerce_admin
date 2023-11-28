e1d203670f7c5af4c9a26396cb884442c19a5b39
John Sasser <cw.john.sasser@gmail.com>
2023-11-28 12:46:19 -0500

    orders page

--- models/product_model.js
+++ models/product_model.js
@@ -1,18 +1,22 @@
+import { Timestamp } from 'mongodb';
 import mongoose, { Schema, model, models } from 'mongoose';
 
-const ProductSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: false },
-  price: { type: Number, required: true },
-  images: { type: Array },
-  category: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Category',
-    required: false,
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    price: { type: Number, required: true },
+    images: { type: Array },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Category',
+      required: false,
+    },
+    properties: {
+      type: Object,
+    },
   },
-  properties: {
-    type: Object,
-  },
-});
+  { timestamps: true }
+);
 
 export const Product = models.Product || model('Product', ProductSchema);
--- pages/orders.js
+++ pages/orders.js
@@ -1,5 +1,60 @@
 import Layout from '@/components/layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Orders() {
-  return <Layout> Orders, duh </Layout>;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/orders').then(res => {
+      let data = res.data;
+      setOrders(data);
+    });
+  }, []);
+
+  return (
+    <Layout>
+      <table className="basic" style={{ fontSize: '.75em' }}>
+        <thead>
+          <tr>
+            <th>Date of Order</th>
+            <th>Payment Confirmed</th>
+            <th>Recipient Details</th>
+            <th>Products</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map(order => (
+              <tr
+                key={order._id}
+                style={{
+                  borderBottom: '1px solid #ccc',
+                }}
+              >
+                <td>{new Date(order.createdAt).toLocaleString()}</td>
+                <td className={order.paid ? 'text-green-600 text-center' : 'text-red-600 text-center'}>
+                  {order.paid ? 'Yes' : 'No'}
+                </td>
+                <td>
+                  <h4>{order.name} </h4>
+                  {order.email} <br />
+                  {order.street} {order.city} {order.zip}
+                </td>
+                <td>
+                  {order.line_items.map(x => (
+                    <tr>
+                      <td>{x.price_data?.product_data.name}</td>
+                      {/* <td style={{ fontWeight: '400' }}>
+                        quantity: {x.quantity}
+                      </td> */}
+                    </tr>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
 }
